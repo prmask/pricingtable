46ca4fc0859bc11326b64ab1f81b8108de00a16c
prmask <prmask@gmail.com>
2021-11-30 18:25:39 +0530

    Pricing Table starting files

---
+++ components/PricingTable/PricingTable.js
@@ -0,0 +1,11 @@
+import classes from './PricingTable.module.scss';
+
+const PricingTable = () => {
+	return (
+		<div className={classes.pricingTable}>
+			<h1>Pricing Table</h1>
+		</div>
+	);
+};
+
+export default PricingTable;
